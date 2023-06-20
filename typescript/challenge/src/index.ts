4e5507fa9a11894ddc2ceef88a1335ab6a1633fd
Nick Murdaugh <nmurdaugh42@gmail.com>
2023-06-20 06:15:41 +0000

    added comments

--- typescript/challenge/src/index.ts
+++ typescript/challenge/src/index.ts
@@ -1,23 +1,39 @@
 const numericArray: number[] = [1, 2, 4, 591, 392, 391, 2, 5, 10, 2, 1, 1, 1, 20, 20]
 // output [[1,1,1,1],[2,2,2], 4,5,10,[20,20], 391, 392,591]
 const answer = (array: any[]) => {
+    // make obj to store numbers and their counts
     interface frequencyOfNumbers {
         [key: number]: number
     }
     const counts: frequencyOfNumbers = {};
+
+    // count each different item in the array and make it a key/value pair; will auto sort ascending
     for (const num of array) counts[num] = (counts[num] | 0) + 1;
+
+    // clone counts obj to a new obj that stores both numbers and arrays of numbers
     interface singleNumbersAndArrays {
         [key: number]: number | number[]
     }
     let finalNumbers: singleNumbersAndArrays = {...counts}
+
+    // create the final array; holds numbers and arrays of numbers
     let finalArray: (number[] | number)[] = []
+
+    // loop through each value
     for ( let [key, value] of Object.entries(finalNumbers)) {
-        if (value > 1) {
+        // check if number occurs more than once
+        if (value > 1) { 
+            // if yes, create array to store number
             let arr: any[] = [];
+            // push number to array number of times equal to count
             for (let step = 0; step < value; step++) arr.push(key);
+            // convert strings to numbers 
             for (let num of arr) num = parseInt(num);
+            // assign array to value
             value = arr
+            // if number only occurs once, assign number to value; makes the final assignment to array easier
         } else value = parseInt(key);
+        // push all values to final array
         finalArray.push(value)
     }
     return finalArray
