3082623148358cf08d071fa3d42b2d45fbc967ef
Nick <nmurdaugh42@gmail.com>
2023-06-19 20:42:41 -0500

    challenge progress: counted frequency of numbers in array

--- typescript/challenge/dist/index.js
+++ typescript/challenge/dist/index.js
@@ -1,8 +1,12 @@
 "use strict";
 const numericArray = [1, 2, 4, 591, 392, 391, 2, 5, 10, 2, 1, 1, 1, 20, 20];
 const answer = (array) => {
-    const sortedNumericArray = array.sort((n1, n2) => n1 - n2);
-    const doubleSortedArray = sortedNumericArray;
+    const counts = {};
+    for (const num of array) {
+        counts[num] = (counts[num] | 0) + 1;
+    }
+    ;
+    let finalNumbers = { ...counts };
 };
 answer(numericArray);
 //# sourceMappingURL=index.js.map
--- typescript/challenge/src/index.ts
+++ typescript/challenge/src/index.ts
@@ -3,14 +3,14 @@ const numericArray: number[] = [1, 2, 4, 591, 392, 391, 2, 5, 10, 2, 1, 1, 1, 20
 const answer = (array: any[]) => {
     interface frequencyOfNumbers {
         [key: number]: number
-    };
+    }
     const counts: frequencyOfNumbers = {};
     for (const num of array) {
         counts[num] = (counts[num] | 0) + 1;
     };
     interface singleNumbersAndArrays {
         [key: number]: number | number[]
-    };
+    }
     let finalNumbers: singleNumbersAndArrays = {...counts}
     // const sortedNumericArray: number[] = array.sort((n1: number, n2: number) => n1 - n2);
     // return sortedNumericArray
