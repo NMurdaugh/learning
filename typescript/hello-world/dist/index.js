e244890660d4db8b779945cb7d5233cbcae9dede
Nick <nmurdaugh42@gmail.com>
2023-06-19 20:48:38 -0500

    delete old files from rename

--- typescript/hello-world/dist/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-const numericArray = [1, 2, 4, 591, 392, 391, 2, 5, 10, 2, 1, 1, 1, 20, 20];
-const answer = (array) => {
-    const sortedNumericArray = array.sort((n1, n2) => n1 - n2);
-    const doubleSortedArray = sortedNumericArray;
-};
-answer(numericArray);
-//# sourceMappingURL=index.js.map
