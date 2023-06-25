fdea927b2ea57c16aaf70786ba596e08599698c1
Nick <nmurdaugh42@gmail.com>
2023-06-25 00:16:33 -0500

    Created kitten cards; implemented props

--- node/kittens/src/main.tsx
+++ node/kittens/src/main.tsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import Card from './Card.tsx'
+import 'tachyons'
+import { kittens } from './kittens'
+// import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <div>
+      <Card id={kittens[0].id} name={kittens[0].name } email={kittens[0].email} />
+      <Card id={kittens[1].id} name={kittens[1].name } email={kittens[1].email} />
+      <Card id={kittens[2].id} name={kittens[2].name } email={kittens[2].email} />
+    </div>
   </React.StrictMode>,
 )
+  
