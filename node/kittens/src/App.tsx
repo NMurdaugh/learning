494ff34d0d092eeffd464de952d59f14362c1f17
Nick <nmurdaugh42@gmail.com>
2023-07-01 16:01:34 -0500

    reset to clean code

--- node/kittens/src/App.tsx
+++ node/kittens/src/App.tsx
@@ -1,4 +1,3 @@
-import { useState } from 'react'
 import CardList from "./CardList"
 import { kittens } from './kittens'
 import SearchBox from './SearchBox'
@@ -6,22 +5,13 @@ import SearchBox from './SearchBox'
 
 
 function App() {
-  const [filteredResults, setFilteredResults] = useState(kittens);
-  const [searchInput, setSearchInput] = useState('');
 
-  const handleSearch = (searchValue: any) => {
-    setSearchInput(searchValue)
-    const filteredKittens = kittens.filter(kitten => {
-      return kitten.name.toLowerCase().includes(searchInput.toLowerCase())
-    })
-    setFilteredResults(filteredKittens)
-  }
 
   return (
     <div className="tc">
       <h1>Kittens</h1>
-      <SearchBox searchChange={handleSearch}/>
-      <CardList kittens={ filteredResults } />
+      <SearchBox />
+      <CardList kittens={ kittens } />
     </div>
   )
 }
--- node/kittens/src/SearchBox.tsx
+++ node/kittens/src/SearchBox.tsx
@@ -1,11 +1,10 @@
-function SearchBox(handleSearch: any) {
+function SearchBox() {
   return (
     <div className="pa2">
       <input
         className="pa3 ba b--green bg-lightest-blue"
         type="search"
         placeholder="Search Kittens"
-        onChange={(e) => handleSearch(e.target.value)}
       />
     </div>
   )
