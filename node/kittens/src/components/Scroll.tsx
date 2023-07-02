d273ddeca92e5f5ad36a5f18558bbb6fe93277ec
Nick <nmurdaugh42@gmail.com>
2023-07-01 22:00:22 -0500

    finished

---
+++ node/kittens/src/components/Scroll.tsx
@@ -0,0 +1,9 @@
+const Scroll = (props: any) => {
+  return (
+    <div style={{overflowY: 'scroll', border: '1px solid black', height: '800px'}}>
+      {props.children}
+    </div>
+  )
+}
+
+export default Scroll
--- node/kittens/src/containers/App.tsx
+++ node/kittens/src/containers/App.tsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
-import CardList from "./CardList";
-import SearchBox from './SearchBox';
+import CardList from "../components/CardList";
+import SearchBox from '../components/SearchBox';
+import Scroll from "../components/Scroll";
 
 
 
@@ -27,8 +28,10 @@ function App() {
     (
     <div className="tc">
       <h1 className="f1">Kittens</h1>
-        <SearchBox searchChange={onSearchChange} />
-      <CardList kittens={ filteredKittens } />
+      <SearchBox searchChange={onSearchChange} />
+      <Scroll>
+        <CardList kittens={filteredKittens} />
+      </Scroll>
     </div>
   )
 }
