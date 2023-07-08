943ae53033823f18c49729a4ee70b9a28a642362
Nick <nmurdaugh42@gmail.com>
2023-07-08 17:06:49 -0500

    Refactored Sorter to use an interface; Built Sortable interface; Made NumbersCollection class to adhere w/ Sortable;

--- typescript/sort/src/index.ts
+++ typescript/sort/src/index.ts
@@ -1,18 +1,3 @@
-class Sorter {
-  constructor(public collection: number[]) {
-  };
+import { Sorter } from './Sorter';
+import { NumbersCollection } from './NumbersCollection';
 
-  sort(): void {
-    const { length } = this.collection;
-
-    for (let i = 0; i < length; i + 1) {
-      for (let j = 0; j < length - i - 1; j + 1) {
-        if (this.collection[j] > this.collection[j + 1]) {
-          const leftSide = this.collection[j];
-          this.collection[j] = this.collection[j + 1];
-          this.collection[j + 1] = leftSide;
-        }
-      }
-    }
-  };
-}
