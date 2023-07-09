573294e2508de96d0ee3245cd5be9cb445bcefaf
Nick <nmurdaugh42@gmail.com>
2023-07-08 20:21:16 -0500

    Ignored dist; made Sorter abstract class to replace interface

--- typescript/sort/dist/index.js
+++ typescript/sort/dist/index.js
@@ -1,21 +1,17 @@
 "use strict";
-class Sorter {
-    constructor(collection) {
-        this.collection = collection;
-    }
-    ;
-    sort() {
-        const { length } = this.collection;
-        for (let i = 0; i < length; i + 1) {
-            for (let j = 0; j < length - i - 1; j + 1) {
-                if (this.collection[j] > this.collection[j + 1]) {
-                    const leftSide = this.collection[j];
-                    this.collection[j] = this.collection[j + 1];
-                    this.collection[j + 1] = leftSide;
-                }
-            }
-        }
-    }
-    ;
-}
+Object.defineProperty(exports, "__esModule", { value: true });
+const NumbersCollection_1 = require("./NumbersCollection");
+const numbers = new NumbersCollection_1.NumbersCollection([50, -5, 8, 37, 0]);
+numbers.sort();
+console.log(numbers);
+// const string = new CharactersCollection('VfRtA');
+// string.sort();
+// console.log(string);
+// const linkedList = new LinkedList();
+// linkedList.add(3);
+// linkedList.add(45);
+// linkedList.add(0);
+// linkedList.add(-9);
+// linkedList.sort()
+// console.log(linkedList);
 //# sourceMappingURL=index.js.map
