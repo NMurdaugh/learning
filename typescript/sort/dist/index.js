7288e9235517cd608adbb29d1da8f999661eab97
Nick <nmurdaugh42@gmail.com>
2023-07-08 20:43:30 -0500

    finished

--- typescript/sort/dist/index.js
+++ typescript/sort/dist/index.js
@@ -1,17 +1,19 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const NumbersCollection_1 = require("./NumbersCollection");
+const CharactersCollection_1 = require("./CharactersCollection");
+const LinkedList_1 = require("./LinkedList");
 const numbers = new NumbersCollection_1.NumbersCollection([50, -5, 8, 37, 0]);
 numbers.sort();
 console.log(numbers);
-// const string = new CharactersCollection('VfRtA');
-// string.sort();
-// console.log(string);
-// const linkedList = new LinkedList();
-// linkedList.add(3);
-// linkedList.add(45);
-// linkedList.add(0);
-// linkedList.add(-9);
-// linkedList.sort()
-// console.log(linkedList);
+const string = new CharactersCollection_1.CharactersCollection('VfRtA');
+string.sort();
+console.log(string);
+const linkedList = new LinkedList_1.LinkedList();
+linkedList.add(3);
+linkedList.add(45);
+linkedList.add(0);
+linkedList.add(-9);
+linkedList.sort();
+console.log(linkedList);
 //# sourceMappingURL=index.js.map
