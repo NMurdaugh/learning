e861749792b533b2bad326065e71809405328163
Nick Murdaugh <nmurdaugh42@gmail.com>
2023-07-11 04:04:23 +0000

    refactored from inheritance to interface

--- typescript/stats_project/src/index.ts
+++ typescript/stats_project/src/index.ts
@@ -1,9 +1,12 @@
 import { MatchReader } from './MatchReader';
 import { MatchKey, MatchResultsKey } from './EnumLibrary';
+import { CsvFileReader } from './CsvFileReader';
 
-const reader = new MatchReader('./football.csv');
+const csvFileReader = new CsvFileReader('football.csv')
 
-reader.read();
+const matchReader = new MatchReader(csvFileReader)
+
+matchReader.load()
 
 const { dateOfMatch, homeTeam, awayTeam, homePoints, awayPoints, matchResults, refereeName } = MatchKey
 
@@ -11,7 +14,7 @@ const { homeWin, awayWin, draw } = MatchResultsKey
 
 let manUnitedWins = 0;
 
-for (let match of reader.data) {
+for (let match of matchReader.matches) {
   if (match[homeTeam] === 'Man United' && match[matchResults] === homeWin) {
     manUnitedWins++
   } else if (match[awayTeam] === 'Man United' && match[matchResults] === awayWin) {
