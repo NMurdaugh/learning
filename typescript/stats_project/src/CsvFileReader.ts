642630cabf2f73380dc446c4cbb9148631a56363
Nick Murdaugh <nmurdaugh42@gmail.com>
2023-07-11 03:20:35 +0000

    Made CsvFileReader an abstract generic class; extended with MatchReader

--- typescript/stats_project/src/CsvFileReader.ts
+++ typescript/stats_project/src/CsvFileReader.ts
@@ -1,13 +1,11 @@
 import fs from 'fs';
-import { stringToDate } from './utils';
-import { MatchKey, MatchResultsKey } from './EnumLibrary';
 
-type MatchData = [Date, string, string, number, number, MatchResultsKey, string]
-
-export class CsvFileReader {
-  data: MatchData[] = [];
+export abstract class CsvFileReader<T> {
+  data: T[] = [];
 
   constructor(public fileName: string) {}
+
+  abstract mapRow(row: string[]): T;
 
   read(): void {
     this.data = fs
@@ -19,16 +17,6 @@ export class CsvFileReader {
       return row.split(',');
       }
     )
-    .map((row: string[]): MatchData => {
-      return [
-        stringToDate(row[0]),
-        row[1],
-        row[2],
-        parseInt(row[3]),
-        parseInt(row[4]),
-        row[5] as MatchResultsKey,
-        row[6],
-      ]
-    })
+    .map(this.mapRow);
   }
 }
