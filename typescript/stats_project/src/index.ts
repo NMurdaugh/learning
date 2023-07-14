d0a838b4bfd5d1a17f44a3ce79c483c7c2485c95
Nick Murdaugh <nmurdaugh42@gmail.com>
2023-07-14 03:44:27 +0000

    Built HtmlReporter class

--- typescript/stats_project/src/index.ts
+++ typescript/stats_project/src/index.ts
@@ -3,17 +3,19 @@ import { MatchReader } from './MatchReader';
 import { WinsAnalyzer } from './analyzers/WinsAnalyzer';
 import { ConsoleReporter } from './reporters/ConsoleReporter';
 import { RunAnalysis } from './RunAnalysis';
+import { HtmlReporter } from './reporters/HtmlReporter';
+
 
 const csvFileReader = new CsvFileReader('football.csv')
 
 const matchReader = new MatchReader(csvFileReader);
 
+matchReader.load();
+
 const analyzer = new WinsAnalyzer('Man United');
 
-const reporter = new ConsoleReporter();
+const reporter = new HtmlReporter();
 
 const analysis = new RunAnalysis(analyzer, reporter);
-
-matchReader.load();
 
 analysis.analyzeAndPrint(matchReader.matches);
--- typescript/stats_project/src/reporters/HtmlReporter.ts
+++ typescript/stats_project/src/reporters/HtmlReporter.ts
@@ -1,1 +1,14 @@
 import { OutputTarget } from '../RunAnalysis';
+import fs from 'fs';
+
+export class HtmlReporter implements OutputTarget {
+  print(report: string): void {
+    const html = `
+    <div>
+      <h1>Analysis Results<h1\>
+      <h1>${report}<h1\>
+    <div\>
+    `;
+    fs.writeFileSync('report.html', html);
+  }
+}
