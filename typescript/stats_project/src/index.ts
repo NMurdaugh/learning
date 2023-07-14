3df1565446cfbb3ce707d82a40854d7f5c50d60f
Nick Murdaugh <nmurdaugh42@gmail.com>
2023-07-14 04:13:58 +0000

    Cleaned up index

--- typescript/stats_project/src/index.ts
+++ typescript/stats_project/src/index.ts
@@ -2,11 +2,9 @@ import { MatchReader } from './MatchReader';
 import { RunAnalysis } from './RunAnalysis';
 
 const matchReader = MatchReader.fromCsv('football.csv');
+const analysisToHtml = RunAnalysis.winsToHtml('Man United');
+const analysisToConsole = RunAnalysis.winsToConsole('Man United');
 
 matchReader.load();
-
-const analysisToConsole = RunAnalysis.winsToConsole('Man United');
-const analysisToHtml = RunAnalysis.winsToHtml('Man United');
-
 analysisToConsole.analyzeAndPrint(matchReader.matches);
 analysisToHtml.analyzeAndPrint(matchReader.matches);
