246fe6204ae722641b0af651182b56a009fa8c2c
Nick <nmurdaugh42@gmail.com>
2023-07-16 18:38:09 -0500

    Made Sync() generic

--- typescript/piecemail/src/models/User.ts
+++ typescript/piecemail/src/models/User.ts
@@ -1,21 +1,25 @@
 import { Eventing } from './Eventing';
+import { Sync } from './Sync';
 
-export interface UserProps {
+export interface IUserProps {
   id?: number;
   name?: string;
   age?: number;
 }
 
+const rootUrl = 'http://localhost:3000/users';
+
 export class User {
   public events: Eventing = new Eventing();
+  public sync: Sync<IUserProps> = new Sync<IUserProps>(rootUrl);
 
-  constructor(private data: UserProps) {}
+  constructor(private data: IUserProps) {}
 
   get(propName: string): string | number {
-    return this.data[propName as keyof UserProps]!;
+    return this.data[propName as keyof IUserProps]!;
   }
 
-  set(update: UserProps): void {
+  set(update: IUserProps): void {
     Object.assign(this.data, update);
   }
 }
