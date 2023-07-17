42647a1d44393b81cafcb8bf98d6de71bd4333ab
Nick <nmurdaugh42@gmail.com>
2023-07-16 19:29:07 -0500

    Extracted Attributes() from User()

--- typescript/piecemail/src/models/User.ts
+++ typescript/piecemail/src/models/User.ts
@@ -12,14 +12,4 @@ const rootUrl = 'http://localhost:3000/users';
 export class User {
   public events: Eventing = new Eventing();
   public sync: Sync<IUserProps> = new Sync<IUserProps>(rootUrl);
-
-  constructor(private data: IUserProps) {}
-
-  get(propName: string): string | number {
-    return this.data[propName as keyof IUserProps]!;
-  }
-
-  set(update: IUserProps): void {
-    Object.assign(this.data, update);
-  }
 }
