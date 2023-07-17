365ed8c0e35169c76f5c1f020c49492cb6985680
Nick <116959833+NMurdaugh@users.noreply.github.com>
2023-07-17 05:20:13 +0000

    Implemented set(), fetch(), save() on User()

--- typescript/piecemail/src/models/User.ts
+++ typescript/piecemail/src/models/User.ts
@@ -1,6 +1,7 @@
 import { Eventing } from './Eventing';
 import { Sync } from './Sync';
 import { Attributes } from './Attributes';
+import { AxiosResponse } from 'axios';
 
 export interface IUserProps {
   id?: number;
@@ -29,5 +30,33 @@ export class User {
 
   get get() {
     return this.attributes.get;
+  }
+
+  set(update: IUserProps): void {
+    this.attributes.set(update);
+    this.events.trigger('change');
+  }
+
+  fetch(): void {
+    const id = this.get('id');
+
+    if (typeof id !== 'number') {
+      throw new Error('Cannot fetch without id');
+    }
+
+    this.sync.fetch(id).then((response: AxiosResponse): void => {
+      this.set(response.data);
+    });
+  }
+
+  save(): void {
+    this.sync
+      .save(this.attributes.getAll())
+      .then((response: AxiosResponse): void => {
+        this.trigger('save');
+      })
+      .catch(() => {
+        this.trigger('error');
+      });
   }
 }
