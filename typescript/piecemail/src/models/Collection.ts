9e14ae580a73401094417230439287352705da57
Nick <116959833+NMurdaugh@users.noreply.github.com>
2023-07-17 09:15:48 +0000

    Made Collection() generic

--- typescript/piecemail/src/models/Collection.ts
+++ typescript/piecemail/src/models/Collection.ts
@@ -1,12 +1,11 @@
 import axios, { AxiosResponse } from 'axios';
 import { Eventing } from './Eventing';
-import { IUserProps, User } from './User';
 
-export class Collection {
-  models: User[] = [];
+export class Collection<T, K> {
+  models: T[] = [];
   events: Eventing = new Eventing();
 
-  constructor(public rootUrl: string) {}
+  constructor(public rootUrl: string, public deserialize: (json: K) => T) {}
 
   get on() {
     return this.events.on;
@@ -18,9 +17,8 @@ export class Collection {
 
   fetch(): void {
     axios.get(this.rootUrl).then((response: AxiosResponse) => {
-      response.data.array.forEach((value: IUserProps) => {
-        const user = User.buildUser(value);
-        this.models.push(user);
+      response.data.array.forEach((value: K) => {
+        this.models.push(this.deserialize(value));
       });
     });
     this.trigger('change');
