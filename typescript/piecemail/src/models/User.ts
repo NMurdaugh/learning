522bbfc50bfbd99f1504b9ef865ab645d88f3715
Nick <nmurdaugh42@gmail.com>
2023-07-16 19:38:26 -0500

    wired Attributes() back in to User()

--- typescript/piecemail/src/models/User.ts
+++ typescript/piecemail/src/models/User.ts
@@ -1,5 +1,6 @@
 import { Eventing } from './Eventing';
 import { Sync } from './Sync';
+import { Attributes } from './Attributes';
 
 export interface IUserProps {
   id?: number;
@@ -12,4 +13,9 @@ const rootUrl = 'http://localhost:3000/users';
 export class User {
   public events: Eventing = new Eventing();
   public sync: Sync<IUserProps> = new Sync<IUserProps>(rootUrl);
+  public attributes: Attributes<IUserProps>;
+
+  constructor(attrs: IUserProps) {
+    this.attributes = new Attributes<IUserProps>(attrs);
+  }
 }
