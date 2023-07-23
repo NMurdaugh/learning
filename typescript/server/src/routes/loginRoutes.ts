ed88df68e5b4cd0903a939d5d4541d526aafbe12
Nick <nmurdaugh42@gmail.com>
2023-07-22 21:47:33 -0500

    Testing /login; extended type of Request

---
+++ typescript/server/src/routes/loginRoutes.ts
@@ -0,0 +1,31 @@
+import { Request, Response, Router } from 'express';
+
+interface requestWithBody extends Request {
+  body: { [key: string]: string | undefined };
+}
+
+const router = Router();
+
+router.get('/login', (req: requestWithBody, res: Response) => {
+  res.send(`
+    <form method="POST">
+      <div>
+        <label for="email-input">Email:</label>
+        <input type="text" id="email-input" name="email">
+      </div>
+      <div>
+        <label for="password-input">Password:</label>
+        <input type="password" id="password-input" name="password">
+      </div>
+      <button>Submit</button>
+    </form>
+  `);
+});
+
+router.post('/login', (req: requestWithBody, res: Response) => {
+  const { email, password } = req.body;
+
+  res.send(email?.toUpperCase() || 'You must provide an email');
+});
+
+export { router };
