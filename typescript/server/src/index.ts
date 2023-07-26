f89758b9dbca2b299e58b72cbb13bcc438c3ecb4
Nick <nmurdaugh42@gmail.com>
2023-07-26 18:22:13 -0500

    Refactored router into a singleton

--- typescript/server/src/index.ts
+++ typescript/server/src/index.ts
@@ -1,8 +1,8 @@
 import bodyParser from 'body-parser';
 import cookieSession from 'cookie-session';
 import express from 'express';
+import { AppRouter } from './AppRouter';
 import './controllers/LoginController';
-import { router as controllerRouter } from './controllers/decorators/controller';
 import { router } from './routes/loginRoutes';
 
 const app = express();
@@ -10,7 +10,7 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieSession({ keys: ['string'] }));
 app.use(router);
-app.use(controllerRouter);
+app.use(AppRouter.getInstance());
 
 app.listen(3000, () => {
   console.log('Listening on port 3000');
