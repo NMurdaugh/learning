b73ca1bd36d419a76e5421e9604e3df81af122bc
Nick <nmurdaugh42@gmail.com>
2023-08-13 19:05:29 -0500

    added typing to dispatch

--- typescript/redux-ts/src/state/action-creators/index.ts
+++ typescript/redux-ts/src/state/action-creators/index.ts
@@ -1,8 +1,10 @@
 import axios from 'axios';
+import { Dispatch } from 'redux';
 import { ActionType } from '../action-types';
+import { Action } from '../actions';
 
 export const searchPackages = (searchTerm: string) => {
-  return async (dispatch: any) => {
+  return async (dispatch: Dispatch<Action>) => {
     dispatch({
       type: ActionType.SEARCH_PACKAGES,
     });
