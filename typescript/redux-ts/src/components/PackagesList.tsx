8124c3ed95b08413b662fc64065eb12a2cdc8685
Nick <nmurdaugh42@gmail.com>
2023-08-14 18:33:45 -0500

    set up search term reactivity and onSubmit behavior

--- typescript/redux-ts/src/components/PackagesList.tsx
+++ typescript/redux-ts/src/components/PackagesList.tsx
@@ -1,8 +1,19 @@
+import { useState } from 'react';
+
 const PackagesList = () => {
+  const [term, setTerm] = useState('');
+
+  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+  };
+
   return (
     <div>
-      <form action=''>
-        <input type='text' />
+      <form onSubmit={onSubmit}>
+        <input
+          value={term}
+          onChange={(e) => setTerm(e.target.value)}
+        />
         <button>Search</button>
       </form>
     </div>
